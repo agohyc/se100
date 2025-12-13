2caa4480b8cb165dac1bf3842c2ecd182a3d4b44
albert-adobebu <albert.adobebu@gmail.com>
2025-12-13 12:16:32 +0800

    Lesson 3 Practice
    Updated practice for lesson 3

---
+++ Lesson3/scripts.js
@@ -0,0 +1,27 @@
+//Data Types
+console.log("Data Types");
+const company = "Adobe";
+console.log(company);
+console.log(company[0]);
+console.log(company.substring(0,2)+company.substring(2));
+console.log(company.length);
+console.log(company.toUpperCase());
+console.log(company.toLowerCase());
+console.log(company.indexOf("be"));
+console.log(company.split(""));
+//Type Conversions
+let age = 25.8;
+age = parseFloat(age);
+console.log(age)
+age = parseInt(age);
+console.log(age);
+let count = toString(age);
+//let stockPrice = 300;
+//let userName = "Alice";
+//const fixedMoney = 2000;
+//console.log("stockPrice = "+stockPrice);
+//console.log("userName = "+userName);
+//console.log("fixedMoney = "+fixedMoney);
+//stockPrice = 350;
+//fixedMoney = 3000;
+//console.log(stockPrice);
